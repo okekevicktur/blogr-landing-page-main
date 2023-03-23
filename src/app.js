ce5a56ae30c0a0396d91bf010312543a7d78ce21
okekevicktur <okekevicktur@gmail.com>
2023-03-23 13:16:55 +0100

    fonts color

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const productMenu= document.getElementById('product');
+const companyMenu= document.getElementById('company');
+const connectMenu= document.getElementById('connect');
+var connectlist= document.getElementById('connectList');
+var productList= document.getElementById('productList');var companylist= document.getElementById('companyList');
+
+window.addEventListener("load", function(){
+    var allUl= this.document.querySelectorAll("ul");
+    allUl.forEach(ul => ul.style.visibility ="hidden")
+   
+});
+
+productMenu.addEventListener('mouseover', function(){
+    listVisibility(productList);
+});
+companyMenu.addEventListener('mouseover', function(){
+    listVisibility(companylist);
+});
+connectMenu.addEventListener('mouseover', function(){
+    listVisibility(connectlist);
+});
+// productList.addEventListener('mouseout', function(){
+//     productList.style.visibility ="hidden";
+// });
+
+function listVisibility(params) {
+
+    
+    var allUl= this.document.querySelectorAll("ul");
+    allUl.forEach(ul => ul.style.visibility ="hidden");
+    params.style.visibility ="visible";
+}
